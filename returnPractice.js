26418a51de6d68e55e564b8f16146dac9693d77d
cameronlASC6 <cameron072002@gmail.com>
2019-07-12 09:46:54 -0400

    Commit's function with return value

---
+++ returnPractice.js
@@ -0,0 +1,8 @@
+function someFunc(){
+    return "Hello World"
+}
+// by default all js functions return a value, if return value is not specified
+//value returns undefined
+const result = someFunc()
+
+console.log (result);
